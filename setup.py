1a1b187414df39cfb6aa70a31fabd409b80db284
Philip Dames <pdames@temple.edu>
2021-02-09 11:07:48 -0500

    Update package configuration to add installation instructions

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="followbot", # Replace with your own username
+    version="1.1.3",
+    author="Zhanteng Xie",
+    author_email="zhanteng.xie@temple.edu",
+    long_description_content_type="text/markdown",
+    url="https://github.com/TempleRAIL/followbot",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
+
