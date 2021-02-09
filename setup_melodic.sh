db37bda37f0b65de620485b041c0cf8bb02211ed
Philip Dames <pdames@temple.edu>
2021-02-09 11:48:19 -0500

    Add setup script for melodic

---
+++ setup_melodic.sh
@@ -0,0 +1,33 @@
+ROS_WS=asi_ws
+
+cd $HOME/$ROS_WS/src
+if [ ! -d kobuki_msgs ]
+then
+    git clone https://github.com/yujinrobot/kobuki_msgs
+fi
+if [ ! -d turtlebot_simulator ]
+then
+    git clone https://github.com/turtlebot/turtlebot_simulator
+fi
+if [ ! -d kobuki_msgs ]
+then
+    git clone https://github.com/turtlebot/turtlebot
+fi
+if [ ! -d kobuki_bumper2pc ] || [ ! -d kobuki_description ]
+then
+    git clone https://github.com/yujinrobot/kobuki
+    mv kobuki/kobuki_bumper2pc .
+    mv kobuki/kobuki_description .
+    rm -rf kobuki
+fi
+if [ ! -d kobuki_gazebo ] || [ ! -d kobuki_gazebo_plugins ]
+then
+    git clone https://github.com/yujinrobot/kobuki_desktop
+    mv kobuki_desktop/kobuki_gazebo .
+    mv kobuki_desktop/kobuki_gazebo_plugins .
+    rm -rf kobuki_desktop
+fi
+
+sudo apt install ros-melodic-yocs-cmd-vel-mux \
+    ros-melodic-joy
+
