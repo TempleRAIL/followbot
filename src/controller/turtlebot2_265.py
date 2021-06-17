061c823e7aa0be6c814709e23c26f63874c6fa4a
PujieXin <33679237+PujieXin@users.noreply.github.com>
2021-06-17 14:45:51 -0400

    Add files via upload

--- src/controller/turtlebot2_265.py
+++ src/controller/turtlebot2_265.py
@@ -9,7 +9,7 @@ import rosparam
 import cv2, cv_bridge
 from sensor_msgs.msg import BatteryState
 from std_msgs.msg import Int8
-from followbot.msg import MGSMeasurement, MGSMeasurements, MGSMarker, MGS_and_Control
+from followbot.msg import MGSMeasurement, MGSMeasurements, MGSMarker
 import math
 from nav_msgs.msg import Odometry
 import message_filters
@@ -21,7 +21,8 @@ from scipy.spatial import distance
 import decimal
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
-from geometry_msgs.msg import Twist,Pose
+from geometry_msgs.msg import Twist, Pose
+
 
 class HoughBundler:
     '''Clasterize and merge each cluster of cv2.HoughLinesP() output
@@ -75,8 +76,8 @@ class HoughBundler:
         u = u1 / (LineMag * LineMag)
 
         if (u < 0.00001) or (u > 1):
-            #// closest point does not fall within the line segment, take the shorter distance
-            #// to an endpoint
+            # // closest point does not fall within the line segment, take the shorter distance
+            # // to an endpoint
             ix = lineMagnitude(px, py, x1, y1)
             iy = lineMagnitude(px, py, x2, y2)
             if ix > iy:
@@ -123,7 +124,7 @@ class HoughBundler:
         orientation = self.get_orientation(lines[0])
 
         # special case
-        if(len(lines) == 1):
+        if (len(lines) == 1):
             return [lines[0][:2], lines[0][2:]]
 
         # [[1,2,3,4],[]] to [[1,2],[3,4],[],[]]
@@ -133,10 +134,10 @@ class HoughBundler:
             points.append(line[2:])
         # if vertical
         if 45 < orientation < 135:
-            #sort by y
+            # sort by y
             points = sorted(points, key=lambda point: point[1])
         else:
-            #sort by x
+            # sort by x
             points = sorted(points, key=lambda point: point[0])
 
         # return first and last point in sorted group
@@ -174,6 +175,7 @@ class HoughBundler:
                 merged_lines_all.extend(merged_lines)
         return merged_lines_all
 
+
 class Controller:
     def __init__(self):
         self.twist = Twist()
@@ -186,39 +188,60 @@ class Controller:
         self.v_max = rospy.get_param('~v_max')
         self.w_max = rospy.get_param('~w_max')
 
-    def find_goal2(self,currentp,current_vel,finalp,final_vel,avg_vel,dist_min):
-        x0,y0 = np.array(currentp)
-        vx0,vy0 = np.array(current_vel) # vx = vel, vy = 0
-        xf,yf = np.array(finalp)
-        vxf,vyf = np.array(final_vel)
-        if xf^2+yf^2 >= dist_min:
-            x_params = cubic_poly_motion_planning(x0,vx0,xf,vxf)
-            y_params = cubic_poly_motion_planning(y0,vy0,yf,vyf)
-            expected_time = convolve_and_sum(x_params,y_params,dist_min)
-            goal_x = x_params * np.array([1,expected_time,expected_time^2,expected_time^3])
-            goal_y = y_params * np.array([1,expected_time,expected_time^2,expected_time^3])
+    def find_goal2(self, currentp, current_vel, finalp, final_vel, avg_vel, dist_min):
+        x0, y0 = np.array(currentp)
+        vx0, vy0 = np.array(current_vel)  # vx = vel, vy = 0
+        xf, yf = np.array(finalp)
+        vxf, vyf = np.array(final_vel)
+        if xf ^ 2 + yf ^ 2 >= dist_min:
+            x_params = self.cubic_poly_motion_planning(x0, vx0, xf, vxf)
+            y_params = self.cubic_poly_motion_planning(y0, vy0, yf, vyf)
+            expected_time = self.convolve_and_sum(x_params, y_params, dist_min)
+            goal_x = x_params * np.array([1, expected_time, expected_time ^ 2, expected_time ^ 3])
+            goal_y = y_params * np.array([1, expected_time, expected_time ^ 2, expected_time ^ 3])
+            vx = x_params * np.array([0, 1, 2 * expected_time, 3 * expected_time ^ 2])
+            vy = y_params * np.array([0, 1, 2 * expected_time, 3 * expected_time ^ 2])
+            if vx > 0:
+                theta = np.arctan(vy / vx)
+            elif vx == 0:
+                theta = np.pi / 2
+            else:
+                if vy >= 0:
+                    theta = np.pi - np.arctan(vy / np.abs(vx))
+                else:
+                    theta = np.pi + np.arctan(np.abs(vy) / np.abs(vx))
         else:
             goal_x = xf
             goal_y = yf
+            if vyf > 0:
+                theta = np.arctan(vxf / vyf)
+            elif vyf == 0:
+                theta = np.pi / 2
+            else:
+                if vyf >= 0:
+                    theta = np.pi - np.arctan(vyf / np.abs(vxf))
+                else:
+                    theta = np.pi + np.arctan(np.abs(vyf) / np.abs(vxf))
+        goal_pose = [goal_x, goal_y, theta]
+        v, w = self.pure_persuit(goal_pose)  # to be changed with proportional control
+        self.twist.linear.x = v
+        self.twist.angular.z = w
+        return self.twist
 
-        v, w = pure_persuit(goal_pose) # to be changed with proportional control
-
-
-
-    def cubic_poly_motion_planning(self,currentp,current_vel,finalp,final_vel,avg_vel):
+    def cubic_poly_motion_planning(self, currentp, current_vel, finalp, final_vel, avg_vel):
         p0 = np.array(currentp)
-        v0= np.array(current_vel) # vx = vel, vy = 0
+        v0 = np.array(current_vel)  # vx = vel, vy = 0
         pf = np.array(finalp)
         vf = np.array(final_vel)
         temp_vec = np.array(finalp - currentp)
-        dt = np.dot(temp_vec,temp_vec)/avg_vel
+        dt = np.dot(temp_vec, temp_vec) / avg_vel
         a0 = p0
         a1 = v0
-        M = np.array([[dt^2,dt^3],[2*dt,3*dt^2]])
-        a2,a3 = np.inv(M)*np.array([[pf - a0 - dt*a1],[vf - a1]])
-        return [a0,a1,a2,a3]
+        M = np.array([[dt ^ 2, dt ^ 3], [2 * dt, 3 * dt ^ 2]])
+        a2, a3 = np.inv(M) * np.array([[pf - a0 - dt * a1], [vf - a1]])
+        return [a0, a1, a2, a3]
 
-    def convolve_and_sum(self,x_a,y_a,dist_min):
+    def convolve_and_sum(self, x_a, y_a, dist_min):
         x_a_convolve = np.convolve(x_a, x_a)
         y_a_convolve = np.convolve(y_a, y_a)
         poly_param = x_a_convolve + y_a_convolve
@@ -227,60 +250,52 @@ class Controller:
         temp = []
         for item in approximate_time:
             if item > 0:
-               temp.append(item)
+                temp.append(item)
         return np.min(temp)
 
-
-    def pure_persuit(self,goal_pose):
+    def pure_persuit(self, goal_pose):
         # pure_persuit is under robot frame.
-        [gx,gy,gtheta] = goal_pose
-        dist = np.sqrt(gx**2+gy**2)
-        # dtheta_1 = gtheta
-        # dtheta_2 = 2*np.pi - gtheta
-        # if dtheta_1 < dtheta_2:
-        #     dtheta = dtheta_1
-        # else:
-        #     dtheta = dtheta_2
-
-        fai = np.arcsin(gy/dist)
+        [gx, gy, gtheta] = goal_pose
+        dist = np.sqrt(gx ** 2 + gy ** 2)
+        fai = np.arcsin(gy / dist)
         case_label = 0
-        if fai>0:
-            if gtheta > fai and gtheta <= np.pi/2-fai:
-                R = (dist/2)/np.sin(fai/2)
+        if fai > 0:
+            if gtheta > fai and gtheta <= np.pi / 2 - fai:
+                R = (dist / 2) / np.sin(fai / 2)
                 v_cmd = self.v_turn
-                w_cmd = v_cmd/R
+                w_cmd = v_cmd / R
                 case_label = 1
-            elif gtheta >= np.pi/2-fai and gtheta <= np.pi/2+fai:
+            elif gtheta >= np.pi / 2 - fai and gtheta <= np.pi / 2 + fai:
                 v_cmd = 0
                 w_cmd = self.max_turning_omega
                 case_label = 2
             else:
-                R = dist/2
+                R = dist / 2
                 v_cmd = self.v_turn
-                w_cmd = 0.5*fai/(dist/self.v_turn)
+                w_cmd = 0.5 * fai / (dist / self.v_turn)
                 case_label = 3
         else:
-            if gtheta >= 3*np.pi/2 and gtheta < 2*np.pi - np.abs(fai):
-                R = (dist/2)/np.abs(np.sin(fai/2))
+            if gtheta >= 3 * np.pi / 2 and gtheta < 2 * np.pi - np.abs(fai):
+                R = (dist / 2) / np.abs(np.sin(fai / 2))
                 v_cmd = self.v_turn
-                w_cmd = -v_cmd/R
+                w_cmd = -v_cmd / R
                 case_label = 4
-            elif gtheta > 3*np.pi/2 - np.abs(fai) and gtheta <= 3*np.pi/2:
+            elif gtheta > 3 * np.pi / 2 - np.abs(fai) and gtheta <= 3 * np.pi / 2:
                 v_cmd = 0
                 w_cmd = -self.max_turning_omega
                 case_label = 5
             else:
-                R = dist/2
+                R = dist / 2
                 v_cmd = self.v_turn
-                w_cmd = 0.5*fai/(dist/self.v_turn)
+                w_cmd = 0.5 * fai / (dist / self.v_turn)
                 case_label = 6
-        print("gx,gy",[gx,gy],"fai",fai,"gtheta",gtheta,"case_label",case_label)
+        print("gx,gy", [gx, gy], "fai", fai, "gtheta", gtheta, "case_label", case_label)
         L = self.wheel_base
-        u = max(v_cmd + L * w_cmd * 1,v_cmd - L * w_cmd * 1)
+        u = max(v_cmd + L * w_cmd * 1, v_cmd - L * w_cmd * 1)
         if u > self.v4:
-            v_cmd = v_cmd * (self.v4/u)
-            w_cmd = w_cmd * (self.v4/u)
-        return (v_cmd,w_cmd,case_label)
+            v_cmd = v_cmd * (self.v4 / u)
+            w_cmd = w_cmd * (self.v4 / u)
+        return (v_cmd, w_cmd, case_label)
 
 
 class Detector:
@@ -290,7 +305,8 @@ class Detector:
         self.img_sub_1 = message_filters.Subscriber('camera/fisheye1/image_raw', Image)
         self.img_sub_2 = message_filters.Subscriber('camera/fisheye2/image_raw', Image)
         # self.odom_sub = rospy.Subscriber("odom", Odometry, self.odom_callback)
-        self.measurements = message_filters.ApproximateTimeSynchronizer([self.img_sub_1,self.img_sub_2], queue_size=5, slop=0.1)
+        self.measurements = message_filters.ApproximateTimeSynchronizer([self.img_sub_1, self.img_sub_2], queue_size=5,
+                                                                        slop=0.1)
         self.measurements.registerCallback(self.measurements_callback)
         self.cmd_vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=1)
         # T265 parameters
@@ -298,73 +314,72 @@ class Detector:
         self.PPY1 = 386.97509765625
         self.Fx1 = 286.221588134766
         self.Fy1 = 287.480102539062
-        self.K1 = np.array([[self.Fx1, 0.  , self.PPX1],
-                            [0.  , self.Fy1, self.PPY1],
-                            [    0.  ,     0.  ,     1.  ]])
+        self.K1 = np.array([[self.Fx1, 0., self.PPX1],
+                            [0., self.Fy1, self.PPY1],
+                            [0., 0., 1.]])
         self.D1 = np.array([-0.0043481751345098, 0.037125650793314, -0.0355393998324871, 0.00577297387644649])
         self.Knew1 = self.K1.copy()
         self.Knew1[(0, 1), (0, 1)] = 1 * self.Knew1[(0, 1), (0, 1)]
         self.R = np.eye(3)
-        self.t = np.array([0.15,-0.03,0.15])
+        self.t = np.array([0.15, -0.03, 0.15])
 
-    def measurements_callback(self,img1,img2):
+    def measurements_callback(self, img1, img2):
         cv_image1 = self.bridge.imgmsg_to_cv2(img1, desired_encoding='bgr8')
         cv_image2 = self.bridge.imgmsg_to_cv2(img2, desired_encoding='bgr8')
         img_undistorted = cv2.fisheye.undistortImage(cv_image1, self.K1, D=self.D1, Knew=self.Knew1)
         img_undistorted = cv2.cvtColor(img_undistorted, cv2.COLOR_BGR2GRAY)
 
         # https://github.com/smidm/opencv-python-fisheye-example/blob/master/fisheye_example.py
-        img_undistorted = cv2.GaussianBlur(img_undistorted, (5,5), 1)
+        img_undistorted = cv2.GaussianBlur(img_undistorted, (5, 5), 1)
         # cv2.imshow("Image window", img_undistorted)
         # cv2.waitKey(0)
-        kernel = np.ones((5,5),np.uint8)
-        erosion = cv2.erode(img_undistorted,kernel,iterations = 1)
+        kernel = np.ones((5, 5), np.uint8)
+        erosion = cv2.erode(img_undistorted, kernel, iterations=1)
         (thresh, im_bw) = cv2.threshold(erosion, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        edges = cv2.Canny(erosion,128,250)
-        temp_lines = probabilistic_hough_line(edges, threshold=20, line_length=40,line_gap=10)
+        edges = cv2.Canny(erosion, 128, 250)
+        temp_lines = probabilistic_hough_line(edges, threshold=20, line_length=40, line_gap=10)
 
         new_lines = []
         for line in temp_lines:
             p0, p1 = line
             if p0[1] > self.Fy1 and p1[1] > self.Fy1:
-                new_lines.append((p0[0],p0[1],p1[0],p1[1]))
+                new_lines.append((p0[0], p0[1], p1[0], p1[1]))
 
         a = HoughBundler()
         foo = a.process_lines(new_lines, edges)
 
-        goal = self.XY_2_XYZ_Goal(foo,img_undistorted)
-        print("goal",goal)
-        [vx,w] = self.goal_pursuit(goal)
+        goal = self.XY_2_XYZ_Goal(foo, img_undistorted)
+        print("goal", goal)
+        [vx, w] = self.goal_pursuit(goal)
         self.twist.linear.x = vx
         self.twist.angular.z = w
         self.cmd_vel_pub.publish(self.twist)
 
-
         fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(20, 20),
-                         sharex=True, sharey=True)
+                                 sharex=True, sharey=True)
         ax = axes.ravel()
 
-        ax[0].imshow(cv_image1,cmap='gray', vmin=0, vmax=255)
+        ax[0].imshow(cv_image1, cmap='gray', vmin=0, vmax=255)
         ax[0].axis('off')
         ax[0].set_title('fisheye', fontsize=20)
 
-        ax[1].imshow(img_undistorted,cmap='gray', vmin=0, vmax=255)
+        ax[1].imshow(img_undistorted, cmap='gray', vmin=0, vmax=255)
         ax[1].axis('off')
         ax[1].set_title('img_undistorted', fontsize=20)
 
-        ax[2].imshow(im_bw,cmap='gray', vmin=0, vmax=255)
+        ax[2].imshow(im_bw, cmap='gray', vmin=0, vmax=255)
         ax[2].axis('off')
         ax[2].set_title('erosion', fontsize=20)
 
-        ax[3].imshow(edges,cmap='gray', vmin=0, vmax=255)
+        ax[3].imshow(edges, cmap='gray', vmin=0, vmax=255)
         ax[3].axis('off')
         ax[3].set_title('edges', fontsize=20)
 
-        ax[3].imshow(edges,cmap='gray', vmin=0, vmax=255)
+        ax[3].imshow(edges, cmap='gray', vmin=0, vmax=255)
         ax[3].axis('off')
         ax[3].set_title('edges', fontsize=20)
 
-        ax[4].imshow(edges * 0,cmap='gray')
+        ax[4].imshow(edges * 0, cmap='gray')
         for line in temp_lines:
             p0, p1 = line
             ax[4].plot((p0[0], p1[0]), (p0[1], p1[1]))
@@ -372,12 +387,12 @@ class Detector:
         ax[4].set_ylim((img_undistorted.shape[0], 0))
         ax[4].set_title('Probabilistic Hough')
 
-        ax[5].imshow(edges * 0,cmap='gray')
+        ax[5].imshow(edges * 0, cmap='gray')
         for line in foo:
             p0, p1 = line
             ax[5].plot((p0[0], p1[0]), (p0[1], p1[1]))
-            ax[5].plot([p0[0],p1[0]],[p0[1],p1[1]],'bo')
-        print("foo[0]",foo[0])
+            ax[5].plot([p0[0], p1[0]], [p0[1], p1[1]], 'bo')
+        print("foo[0]", foo[0])
         ax[5].set_xlim((0, img_undistorted.shape[1]))
         ax[5].set_ylim((img_undistorted.shape[0], 0))
         ax[5].set_title('Merged Probabilistic Hough')
@@ -387,21 +402,21 @@ class Detector:
         # cv2.imshow("Image window", skeleton)
         # cv2.waitKey(0)
 
-    def theta_filter(self,lines):
+    def theta_filter(self, lines):
         temp_lines = []
         for line in lines:
             if line[1][0] - line[0][0] > 0.00001:
-                k = (line[0][1]-line[1][1])/(line[1][0] - line[0][0])
+                k = (line[0][1] - line[1][1]) / (line[1][0] - line[0][0])
             else:
                 k = np.Inf
-            print("line",line,"k",k)
+            print("line", line, "k", k)
             if np.abs(k) >= 1.73:
                 temp_lines.append(line)
         return temp_lines
 
-    def XY_2_XYZ_Goal(self,lines,img_undistorted):
+    def XY_2_XYZ_Goal(self, lines, img_undistorted):
         height, width = img_undistorted.shape
-        center_point = [(np.floor(width/2),height-1)]
+        center_point = [(np.floor(width / 2), height - 1)]
         points_list = []
         lines = self.theta_filter(lines)
         if len(lines) == 0:
@@ -410,18 +425,18 @@ class Detector:
             for line in lines:
                 points_list.append(line[0])
                 points_list.append(line[1])
-            DIST = distance.cdist(np.array(points_list),np.array(center_point))
+            DIST = distance.cdist(np.array(points_list), np.array(center_point))
             index = np.where(DIST == DIST.min())
-            print("lines",lines)
+            print("lines", lines)
             # print("DIST",DIST,"index",index,"center_point",center_point)
             XY_goal = points_list[np.int(index[0])]
-            print("XY_goal",XY_goal)
+            print("XY_goal", XY_goal)
             Y = -150
-            print("self.Fy1/Y",self.Fy1*Y,"XY_goal[1]-self.PPY1",XY_goal[1]-self.PPY1)
-            Z = (self.Fy1*Y)/(XY_goal[1]-self.PPY1)
-            print("Z",Z)
-            X = (XY_goal[0]-self.PPX1)/(self.Fx1/Z)
-            result = np.array([X,Y,Z])/1000.0
+            print("self.Fy1/Y", self.Fy1 * Y, "XY_goal[1]-self.PPY1", XY_goal[1] - self.PPY1)
+            Z = (self.Fy1 * Y) / (XY_goal[1] - self.PPY1)
+            print("Z", Z)
+            X = (XY_goal[0] - self.PPX1) / (self.Fx1 / Z)
+            result = np.array([X, Y, Z]) / 1000.0
         return result
 
     # def odom_callback(self,odom_msg):
@@ -440,9 +455,9 @@ class Detector:
     #                                      odom_msg.pose.angular.z,
     #                                      odom_msg.pose.angular.w])
     #     # transfer to 3D
-    def goal_pursuit(self,goal_pose):
-        [gx,gy,gtheta] = goal_pose
-        dist = np.sqrt(gx**2+gy**2)
+    def goal_pursuit(self, goal_pose):
+        [gx, gy, gtheta] = goal_pose
+        dist = np.sqrt(gx ** 2 + gy ** 2)
         if dist < 0.3:
             v_fix = 0
             w = 0
@@ -451,13 +466,13 @@ class Detector:
                 v_fix = 0.5
                 w = 0
             else:
-                line_center = np.array([gx/2.0,gy/2.0])
-                k1 = gy/gx
-                k2 = -gx/gy
-                b = gy/2.0 - k2 * gx/2.0
-                x2 = -b/k2
-                circle_center = np.array([x2,0])
-                k3 = gy/(gx - x2)
+                line_center = np.array([gx / 2.0, gy / 2.0])
+                k1 = gy / gx
+                k2 = -gx / gy
+                b = gy / 2.0 - k2 * gx / 2.0
+                x2 = -b / k2
+                circle_center = np.array([x2, 0])
+                k3 = gy / (gx - x2)
                 dtheta = np.arctan(k3)
                 if x2 > 0 and dtheta > 0:
                     dtheta = np.pi - dtheta
@@ -467,17 +482,15 @@ class Detector:
                 dtheta = np.abs(dtheta)
                 v_fix = 0.5
                 route_dist = np.abs(x2) * dtheta / v_fix
-                w = dtheta/(route_dist/v_fix)
+                w = dtheta / (route_dist / v_fix)
                 if x2 > 0:
                     w = np.abs(w)
                 else:
                     w = -np.abs(w)
-        return v_fix,w
+        return v_fix, w
 
 
-
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     rospy.init_node('line_detector')
     detector = Detector()
     rospy.spin()
